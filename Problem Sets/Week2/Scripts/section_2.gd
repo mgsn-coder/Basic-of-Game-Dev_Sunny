8483613660364e8acb236577abeff33b8568568f
SUNNIE <chattawan.par@gmail.com>
2026-09-12 20:17:28 +0300

    Submitted Problem Set Sec 2 & 3
    I skipped section 1 and the challenge.  I cannot find solutions from GDQuest 1-17. I uploaded my progress so far and continue figuring them out.

--- Problem Sets/Week2/Scripts/section_2.gd
+++ Problem Sets/Week2/Scripts/section_2.gd
@@ -2,17 +2,37 @@ extends Node
 
 #Write a function greet() that prints "Hello, adventurer!" to the console.
 
+func greet():
+	print("Hello, adventure!")
+
 #Write a function add_score(points) that takes one parameter and returns the current score plus points.
 var score := 5
 
+func add_score(points):
+	score += points
+	print(score)
 
 #Write a function is_alive(hp) that takes an integer hp and returns true if hp is greater than 0, and false otherwise.
 var hp := 80
 
+func is_alive(hp):
+	if hp > 0:
+		true
+	else: 
+		false # I'm not sure about else
+
 #Write a function heal(current_hp, amount, max_hp) that returns the new HP after healing, but never lets it exceed max_hp.
 var max_hp := 100
+var health
 
-
+func heal(current_hp, amount, max_hp):
+	current_hp = health + amount
+	if current_hp > max_hp:
+		print(max_hp)
+	
+	
 #What keyword built into every Godot Node script runs once when the node enters the scene tree? Write a stub for it that prints a number of your choice.
+# I don't know... T3T
 
 #How often does the _process() function run?
+# 60 frame/second, looping forever
--- Problem Sets/Week2/Scripts/section_3.gd
+++ Problem Sets/Week2/Scripts/section_3.gd
@@ -2,6 +2,24 @@ extends Node
 
 # Write a for loop using range() that prints the numbers 1 through 10 (inclusive).
 
+var number
+
+func run():
+	for number in range(10):
+		print() # what is inclusive?
+
 #rewrite it as a while loop
 
+	while number > 10:
+		print(number)
+		number += 1
+
 #Given an array scores = [10, 9, 7, 10, 6], write a for loop that calculates and prints the total sum.
+
+func calculate_sum():
+	var array = [10,9,7,10,6]
+	var element
+	
+	for number in range(array):
+		element = array[1+2+3+4+5] #culcurate the sum
+		print(element) #print the sum
