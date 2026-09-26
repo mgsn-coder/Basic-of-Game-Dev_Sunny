e815f0f94452759490edba12df3261422cd5936a
SUNNIE <chattawan.par@gmail.com>
2026-09-26 16:29:59 +0300

    Added Player Moving Direction
    Assigned player facing the direction it move.

--- Godot Project/gd-beginner-ttr/Scripts/player.gd
+++ Godot Project/gd-beginner-ttr/Scripts/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,9 +15,18 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get input direction: -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
+	
+	# Flip the sprite
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+	
+	
+	
+	# Apply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
